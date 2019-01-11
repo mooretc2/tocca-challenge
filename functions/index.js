a09754d8011ff8f685a1d8fde013fd652dc8c5e0
Tyler Moore <mooretc2@miamioh.edu>
2019-01-10 19:58:12 -0800

    Adding express and hello world endpoint

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,7 @@
 const functions = require('firebase-functions'),
-	admin = require('firebase-admin');
+	admin = require('firebase-admin'),
+	express = require('express'),
+	app = express();
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -7,3 +9,13 @@ const functions = require('firebase-functions'),
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+app.get('profiles', function(req, res) {
+	res.send("Hello World!");
+});
+
+const api = functions.https.onRequest(app);
+
+module.exports = {
+	api
+}
