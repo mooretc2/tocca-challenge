93b1b76669b9c5c9d36a25acd9418aa862aaa975
Tyler Moore <mooretc2@miamioh.edu>
2019-01-10 22:18:29 -0800

    Fixing linter warning

--- functions/index.js
+++ functions/index.js
@@ -4,7 +4,7 @@ const functions = require('firebase-functions'),
 	app = express();
 	
 admin.initializeApp(functions.config().firebase);
-database = admin.database();
+const database = admin.database();
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -20,7 +20,7 @@ app.get('/:start/:end', (req, res) => {
 	if(end < start || end - start > 50){
 		res.status(400).send("Invalid start/end values");
 	}
-	await database.ref('tocca-project/user_profile').startAt(start).endAt(end-1)
+	database.ref('tocca-project/user_profile').startAt(start).endAt(end-1)
 		.then((result) => {res.json(result)};
 });
 
