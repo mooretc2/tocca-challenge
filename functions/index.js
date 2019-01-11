fa91b10fe21bb27e58289a6dfd1fb29ae726701a
Tyler Moore <mooretc2@miamioh.edu>
2019-01-10 21:42:34 -0800

    Adding database query, fixing lint warning

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,8 @@
 const functions = require('firebase-functions'),
 	admin = require('firebase-admin'),
 	express = require('express'),
-	app = express();
+	app = express(),
+	database = firebase.database();
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -10,18 +11,20 @@ const functions = require('firebase-functions'),
 //  response.send("Hello from Firebase!");
 // });
 
-app.get('/:start/:end', function(req, res) {
+app.get('/:start/:end', (req, res) => {
 	let start = req.params.start,
 		end = req.params.end;
 	
 	if(end < start || end - start > 50){
-		res.sendStatus(400).send("Invalid start/end values")
+		res.status(400).send("Invalid start/end values");
 	}
-	res.send(start+" "+end);
+	let result = database.ref('tocca-project/user_profile').orderByKey().startAt(start).endAt(end-1);
+	res.json(result);
 });
 
-app.get('/', function(req, res) {
-	res.send("Hello World!");
+app.get('/', (req, res) => {
+	let result = database.ref('tocca-project/user_profile').orderByKey().startAt(0).endAt(49);
+	res.json(result);
 });
 
 const profiles = functions.https.onRequest(app);
