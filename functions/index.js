8da279913215d14fe01a7c4263a2c40359d05ee3
Tyler Moore <mooretc2@miamioh.edu>
2019-01-10 22:10:47 -0800

    Fixing error with database query

--- functions/index.js
+++ functions/index.js
@@ -2,6 +2,9 @@ const functions = require('firebase-functions'),
 	admin = require('firebase-admin'),
 	express = require('express'),
 	app = express();
+	
+admin.initializeApp(functions.config().firebase);
+database = admin.database();
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -17,12 +20,12 @@ app.get('/:start/:end', (req, res) => {
 	if(end < start || end - start > 50){
 		res.status(400).send("Invalid start/end values");
 	}
-	let result = functions.database.ref('tocca-project/user_profile').startAt(start).endAt(end-1);
+	let result = database.ref('tocca-project/user_profile').startAt(start).endAt(end-1);
 	res.json(result);
 });
 
 app.get('/', (req, res) => {
-	let result = functions.database.ref('tocca-project/user_profile').startAt(0).endAt(49);
+	let result = database.ref('tocca-project/user_profile').startAt(0).endAt(49);
 	res.json(result);
 });
 
