3b6db286a40cdf5209402ccfacf7f0e7a5837787
Tyler Moore <mooretc2@miamioh.edu>
2019-01-10 22:22:38 -0800

    Fixing linter error

--- functions/index.js
+++ functions/index.js
@@ -21,13 +21,13 @@ app.get('/:start/:end', (req, res) => {
 		res.status(400).send("Invalid start/end values");
 	}
 	database.ref('tocca-project/user_profile').startAt(start).endAt(end-1)
-		.then((result) => {res.json(result)})
+		.then((result) => {return res.json(result)})
 		.catch((error) => {res.status(500).send(error)});
 });
 
 app.get('/', (req, res) => {
 	database.ref('tocca-project/user_profile').startAt(0).endAt(49)
-		.then((result) => {res.json(result)})
+		.then((result) => {return res.json(result)})
 		.catch((error) => {res.status(500).send(error)});
 });
 
